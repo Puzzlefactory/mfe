941ae3b2e3fe99f63cfb5e5aec3b2c58bbe02823
Alan Bryant <alan@puzzlefactory.io>
2021-01-17 08:35:07 -0500

    Expanded comments on app.js to get build to kick off

--- container/src/App.js
+++ container/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MarketingApp from "./components/MarketingApp";
 
 // Main app file
+// This is a container that should hold the rest of the MFEs
 export default () => {
   return <div>
     <h1>Hi There!</h1>
