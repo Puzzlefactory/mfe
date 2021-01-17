c0abb7cf2a7563cb6006ab3ce958c7eb5738ef11
Alan Bryant <alan@puzzlefactory.io>
2021-01-17 08:43:25 -0500

    minor update

--- container/src/App.js
+++ container/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import MarketingApp from "./components/MarketingApp";
 
 // Main app file
-// This is a container that should hold the rest of the MFEs
 export default () => {
   return <div>
     <h1>Hi There!</h1>
