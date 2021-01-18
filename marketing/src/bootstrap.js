f14f533c9e321f7ccaf6ae5471d85ff5db71dcb0
Alan Bryant <alan@puzzlefactory.io>
2021-01-18 12:24:08 -0500

    maybe this will fix paths issue?

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -4,8 +4,10 @@ import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el, { onNavigate, defaultHistory }) => {
-  const history = defaultHistory || createMemoryHistory();
+const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+  const history = defaultHistory || createMemoryHistory({
+    initialEntries: [initialPath],
+  });
   if (onNavigate) {
     history.listen(onNavigate);
   }
@@ -28,6 +30,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
+    console.log(document.location.pathname)
     mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
