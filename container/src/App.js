0aecacf9c3844eaaf5013f443515d90bb20e3bb9
Alan Bryant <alan@puzzlefactory.io>
2021-01-19 10:30:17 -0500

    added a progressbar

--- container/src/App.js
+++ container/src/App.js
@@ -2,9 +2,8 @@ import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
-// import MarketingApp from "./components/MarketingApp";
-// import AuthApp from "./components/AuthApp";
 import Header from './components/Header';
+import Progress from "./components/Progress";
 
 const MarketingLazy = lazy(() => import('./components/MarketingApp'));
 const AuthLazy = lazy(() => import('./components/AuthApp'));
@@ -23,7 +22,7 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header/>
-          <Suspense fallback={<div>Loading...</div>}>
+          <Suspense fallback={<Progress />}>
             <Switch>
               <Route path="/auth" component={AuthLazy}/>
               <Route path="/" component={MarketingLazy}/>
