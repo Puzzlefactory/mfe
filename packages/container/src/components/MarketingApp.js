7ac5e680bf4dea4915b3323350466a9f55925b3d
Alan Bryant <alan@puzzlefactory.io>
2021-01-23 11:52:17 -0500

    Initial setup

---
+++ packages/container/src/components/MarketingApp.js
@@ -0,0 +1,27 @@
+import { mount } from 'marketing/MarketingApp';
+import React, { useRef, useEffect } from "react";
+import { useHistory } from 'react-router-dom';
+
+export default () => {
+  const ref = useRef(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    const { onParentNavigate } = mount(ref.current, {
+      initialPath: history.location.pathname,
+      onNavigate: ({pathname: nextPathName}) => {
+        const { pathname } = history.location;
+        if (pathname !== nextPathName) {
+          console.log('Navigation has occured', nextPathName);
+          history.push(nextPathName);
+        }
+
+      }
+    });
+
+    history.listen(onParentNavigate);
+
+  }, []);
+
+  return <div ref={ref} />;
+};
