53499affca00d8c3388cb0e8b48042ba684400ec
Alan Bryant <alan@puzzlefactory.io>
2021-02-13 09:57:05 -0500

    updated libraries except for dashboard

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -27,6 +27,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath, onSignIn }) => {
 // If we are in development and in isolation,
 // call mount immediately
 if (process.env.NODE_ENV === 'development') {
+  console.log('in development mode');
   const devRoot = document.querySelector('#_auth-dev-root');
 
   if (devRoot) {
